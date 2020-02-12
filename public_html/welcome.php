f84349f413c22b13fc6ae136316ee41354e0f4c3
jyoussef98 <jy353@njit.edu>
2020-02-11 21:47:21 -0500

    all files under my public_html
    copy of all files in /var/www/html/PPG/public_html

---
+++ public_html/welcome.php
@@ -0,0 +1,12 @@
+<?php
+	include('session.php');
+?>
+
+<html>
+	<head>
+		<title> You're In. </title>
+	</head>
+	<body>
+	<h1> Welcome U Ugly Bicth <?php echo $login_session;?> </h1>
+		<h2><a href="register.php">New Account</a></h2>
+</html>
