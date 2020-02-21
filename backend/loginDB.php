fa2bc2a3003d2315fc7deac4ff5d2ac7c08848ac
root <jy353@njit.edu>
2020-02-20 20:58:04 -0500

    updated dmzCall.php to create a connection from local db to dmz.

--- backend/loginDB.php
+++ backend/loginDB.php
@@ -1,10 +1,10 @@
 <?php 
 	$db_host = 'localhost';
-	$db_username = 'nemo';
-	$db_password = 'dory123';
-	$db_name = 'reef';
+	$db_username = 'admin';
+	$db_password = 'adminpassword';
+	$db_name = 'ingr';
 
-	$mydb = new mysqli($db_host, $db_username, $db_password, $db_name);	
+	$mydb = new mysqli($db_host, $db_username,$db_password, $db_name);	
 	
 	if(!$mydb)
 	{
