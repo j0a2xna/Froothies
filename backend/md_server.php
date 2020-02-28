dce7f47b3a68c8508c42216a3d18771bb82912cf
jyoussef98 <jy353@njit.edu>
2020-02-27 20:14:36 -0500

    update md and dmz server
    temp add mydb connection to md server to troubleshoot connection
    
    created new function in dmz server to separate curl function out of process request so that they dont bump wires

--- backend/md_server.php
+++ backend/md_server.php
@@ -3,6 +3,20 @@
     require_once('path.inc');
     require_once('rabbitMQLib.inc');
 
+    $db_host = 'localhost';
+	$db_username = 'admin';
+	$db_password = 'adminpassword';
+	$db_name = 'ingr';
+
+	$mydb = new mysqli($db_host, $db_username,$db_password, $db_name);	
+	
+	if(!$mydb)
+	{
+		die("Connection failed: ". msqli_connect_error());
+	}else{
+		echo "Successful connection";
+	}
+
 
     $client = new rabbitMQClient("RMQ_Server.ini","RMQ_Server");
 
