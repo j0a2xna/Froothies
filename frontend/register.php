2e954ed50efcd704fc7dbb324ead17627d766fbb
orquidiadr <orquidiadr01@gmail.com>
2020-02-27 19:12:30 -0500

    removing a bunch of head and master :S words

--- frontend/register.php
+++ frontend/register.php
@@ -8,19 +8,12 @@
 	$username = '';
 	$password = '';
 	$request = array();
-<<<<<<< HEAD
+
 
 	if(isset($_POST['username'])){
 		$username = $_POST['username'];
 		$password = $_POST['password'];
 
-=======
-
-	if(isset($_POST['username'])){
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-
->>>>>>> 0e79906e1c1d969cb94b8afec65c200c4952a662
    		$request['username'] = $username;
 		$request['password'] = $password;
 
@@ -34,7 +27,6 @@
 	function process_response($response){
 		var_dump($response);
 		if($response == "registered"){
-<<<<<<< HEAD
 			$suc_register = "You were successfuly registered. Please log in.";
 			echo "<script type='text/javascript'>
 				alert('$suc_register');
@@ -46,11 +38,6 @@
 				alert('$bad_register');
 				window.location = 'register.php';
 			      </script>";
-=======
-				echo "you have successfully registered\n".PHP_EOL;
-		}else{
-				echo "sorry username taken\n".PHP_EOL;
->>>>>>> 0e79906e1c1d969cb94b8afec65c200c4952a662
 		}
 	}	
 ?>
@@ -72,22 +59,12 @@
 				</br></br>
 					Password: 
 					<input type="password" name="password" id="box" autocomplete="off" required/>
-<<<<<<< HEAD
-				</br></br>
-					Email address:
-					<input type="email" name="email" id="box" autocomplete="off" required/>
-				</br></br>
-					<input type="Submit" value="Register" name="register"/>
-				</form>			
-=======
 				</br></br>
 					Email address:
 					<input type="email" name="email" id="box" autocomplete="off" required/>
 				</br></br>
 					<input type="Submit" value="Register" name="register"/>
 				</form>
-			
->>>>>>> 0e79906e1c1d969cb94b8afec65c200c4952a662
 					<div style="font-size:12px;color:#000000;margin-top:10px">
 						Already have an account? <a href="index.php">Log in here!</a>
 					</div>
@@ -96,7 +73,3 @@
 	</div>
 	</body>
 </html>
-<<<<<<< HEAD
-=======
-
->>>>>>> 0e79906e1c1d969cb94b8afec65c200c4952a662
