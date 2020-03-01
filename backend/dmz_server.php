f84a155aa24a86d468d666f5e0ac26619d8af26f
jyoussef98 <jy353@njit.edu>
2020-03-01 17:23:41 -0500

    debugging parsed error

--- backend/dmz_server.php
+++ backend/dmz_server.php
@@ -21,7 +21,7 @@
         $APP_KEY = 'c122653d4096a00999bf36f4e1d4958e';
         $ch = curl_init();
         
-        $url = "https://api.edamam.com/api/food-database/parser?ingr='$food'&category=generic-foods&category-label=food&app_id='$APP_ID'&app_key='$APP_KEY'";
+        $url = "https://api.edamam.com/api/food-database/parser?ingr='.$food.'&category=generic-foods&category-label=food&app_id='$APP_ID'&app_key='$APP_KEY'";
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -33,11 +33,12 @@
         $array = json_decode($jsonData, true);
         curl_close($ch);
 
-        $response['name'] = $array['parsed'][0]['food']['label'];
-        $response['cal'] = $array['parsed'][0]['food']['nutrients']['ENERC_KCAL'];
-        $response['pro'] = $array['parsed'][0]['food']['nutrients']['PROCNT'];
-        $response['fat'] = $array['parsed'][0]['food']['nutrients']['FAT'];
-        $response['carb'] = $carb = $array['parsed'][0]['food']['nutrients']['CHOCDF'];
+        #$response['name'] = $array['parsed'][0]['food']['label'];
+        #$response['cal'] = $array['parsed'][0]['food']['nutrients']['ENERC_KCAL'];
+        #$response['pro'] = $array['parsed'][0]['food']['nutrients']['PROCNT'];
+        #$response['fat'] = $array['parsed'][0]['food']['nutrients']['FAT'];
+        #$response['carb'] = $carb = $array['parsed'][0]['food']['nutrients']['CHOCDF'];
+        $response = $array;
 
         return $response;
     }
