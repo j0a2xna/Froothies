1eaaf26cb2ca5e79aa514fcd592080dd1118ff2b
jyoussef98 <jy353@njit.edu>
2020-03-04 21:21:34 -0500

    updating recipe.php
    >> added javascript to append fruit to smoothie list
    
    HOWEVER
    
    request only records the first input
    >> also added if statement to only send form when it is submitted

--- frontend/recipe.php
+++ frontend/recipe.php
@@ -9,63 +9,69 @@
   $client = new rabbitMQClient("recipe.ini", "recipe_server");
    
     if(isset(_POST['submit'])){
-      $recipe=$_POST['Recipe'];
-      $fruit=$_POST['Fruit'];
-      $vegetable=$_POST['Vegetables'];
-      $protein=$_POST ['Protein'];
-      $base=$_POST ['Base'];
+      $recipe=$_POST['recipe'];
+      $fruit=$_POST['fruit'];
+      $vegetable=$_POST['veggies'];
+      $protein=$_POST ['protein'];
+      $base=$_POST ['base'];
+
+      $fill = TRUE;
     }
 	 
-	 
-    $request = array();
-    $request['username'] = $username;
-    $request['recipe'] = $recipe;
-    $request['fruit'] = $fruit;
-    $request['vegetable'] = $vegetable;
-    $request['protein'] = $protein;
-    $request['base'] = $base;
-    
-    $response = $client->send_request($request);
-    echo "$response";
-	
+    if($fill){
+      $request = array();
+      $request['username'] = $username;
+      $request['recipe'] = $recipe;
+      $request['fruit'] = $fruit;
+      $request['vegetable'] = $vegetable;
+      $request['protein'] = $protein;
+      $request['base'] = $base;
+      
+      $response = $client->send_request($request);
+      echo "$response";
+    }
+      
 ?>
 
 <html>
 <head>
-<style>
-h2 {
-   color: blue;
-
-   }
- h3 {
-   color: tomato;
-   }
- b{
-    color: tomato;
-
-        }
-
-
- </style>
-
-
+    <style>
+      .fDiv{
+        float: left;
+      }
+      .list{
+        float: right;
+      }
+    </style>
+    <script> 
+      document.getElementById("add").onclick = function(){
+        var text = document.getElementById("ingr").value;
+        var li = "<li>" + text + "</li>";
+        document.getElementById("smoothie").appendChild(li);
+      }
+    </script>
+</head>
 <center><h2>Lets Make Some Froothies!!!!</h2></center><br><br>
 
 <body>
-<form action="<?php echo $_SERVER['PHP_SELF'];?>" method= "post">
+  <div id="fDiv">
+      <form action="<?php echo $_SERVER['PHP_SELF'];?>" method= "post">
 
-  <b>Give your Recipe a Name:</label></b> <input type="text" id="RecipeName" name="Recipe"><br>
+        <b>Give your Recipe a Name:</label></b> <input type="text" id="RecipeName" name="recipe"><br>
 
-  <h3>Fruit:</label><br> <input type="text" id="FruitName" name="Fruit" > <button class="btn add" onclick="alert('Added!')">ADD</button><br>
-  
-  <h3>Vegetables:</label><br> <input type="text" id="VegetableName" name="Vegetables"> <button class="btn add" onclick="alert('Added!')">ADD</button><br>
-  
-  <h3>Protein:</label><br> <input type="text" id="ProteinName" name="Protein">  <button class="btn add" onclick="alert('Added!')">ADD</button><br>
-  
-  <h3>Base:</label><br> <input type="text" id="BaseName" name="Base"> 
-  <input type="Submit" value="ADD RECIPE" name="submit"/>
-</form> 
-
+        <h3>Fruit:</label><br> <input type="text" id="ingr" name="fruit"> <input type="button" value="Add To Smoothie" id="add"/><br>
+        
+        <h3>Veggies:</label><br> <input type="text" id="ingr" name="veggies"> <input type="button" value="Add To Smoothie" id="add"/><br>
+        
+        <h3>Protein:</label><br> <input type="text" id="ingr" name="protein"> <input type="button" value="Add To Smoothie" id="add"/><br>
+        
+        <h3>Base:</label><br> <input type="text" id="ingr" name="base"> <input type="button" value="Add To Smoothie" id="add"/><br>
+        <input type="Submit" value="ADD RECIPE" name="submit"/>
+      </form> 
+    </div>
+    <div id="list">
+      <ul id="smoothie"></ul>
+    </div>
 
 </body>
 </html>
