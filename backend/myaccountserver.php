f91aced46487182db4683492faec540f987c05ca
root <ts459@njit.edu>
2020-03-05 19:48:43 -0500

    myaccount is now working with rabbitmq(the devil). if user has recipes, it will load all the recipes. If user does not have recipe, it will ask them to go make a smoothie in makesmoothie.php

--- backend/myaccountserver.php
+++ backend/myaccountserver.php
@@ -26,16 +26,74 @@
 		$conn = connectDB();
 		$sql = "SELECT recipeName, fruits, veggies, protein, base FROM $username";
 		$result = mysqli_query($conn, $sql);
-		
-		if(mysqli_num_rows($result) > 0){
-			$row = mysqli_fetch_assoc($result);
-			# $row = mysqli_fetch_array($result);
-			echo $row["recipeName"];
-			return $row;
-		}
-		
 
-	}
+		//as long as there is a row, insert it into the results array
+		if(mysqli_num_rows($result) > 0){
+			$results = array();
+			while($row = mysqli_fetch_array($result)){
+				$results[] = $row;
+			}
+			print_r($results); //prints array in cli
+
+
+			foreach($results as $test) {
+				echo $test[0] . '<br>';
+				echo $test[1] . '<br>';
+				echo $test[2] . '<br>';
+				echo $test[3] . '<br>';
+				echo $test[4] . '<br>';
+			}
+			return $results; //return the array to myaccount.php ->process_response()
+		}
+
+		// if user has not created any arrays, they will be asked to create one ->makesmoothie.php
+		else{
+			echo "<h1>OOPS!</h1>";
+			echo "<h2> $username --> You have not created any recipes! </h2>";
+		}
+
+} 
+			//echo implode(',', $results);
+/*
+			foreach($results as $value){
+				echo $value;
+			}
+return $results; */
+/*
+		if(mysqli_num_rows($result) > 0){
+			echo "<h1>Welcome $username!!</h1>" .PHP_EOL;
+			echo "<td>".$row["recipeName"]."</td";
+			while($row = mysqli_fetch_assoc($result)){
+                        echo "<table>";
+                        echo "<tr>";
+                        echo "<td>Name: </td>";
+                        echo "<td>".$row["recipeName"]."</td>";
+                        echo "</tr>";
+
+                        echo "<tr>";
+                        echo "<td>Fruits: </td>";
+                        echo "<td>".$row["fruits"]."</td>";
+                        echo "</tr>";
+
+                        echo "<tr>";
+                        echo "<td>Veggies: </td>";
+                        echo "<td>".$row["veggies"]."</td>";
+                        echo "</tr>";
+
+                        echo "<tr>";
+                        echo "<td>Protein: </td>";
+                        echo "<td>".$row["protein"]."</td>";
+                        echo "</tr>";
+
+                        echo "<tr>";
+                        echo "<td>Base</td>";
+                        echo "<td>".$row["base"]."</td>";
+                        echo "</tr>";
+
+                        echo "</table>";
+
+                        echo "******************************************";
+			}*/
 
 	$server = new rabbitMQServer("account.ini","accountServer");
 	$server->process_requests('requestProcessor');
