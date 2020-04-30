dc67a36523c0e0421680d88705277b93a7072604
orquidiadr <33134581+orquidiadr@users.noreply.github.com>
2020-04-30 16:50:17 -0400

    Social media - facebook button
    Added the facebook button to share our page froothies.com
    In terms of css, it's at the bottom of the screen.
    Still need to figure out how to attach it to the smoothies.

--- frontend/welcome.php
+++ frontend/welcome.php
@@ -205,6 +205,32 @@ if(isset($_SESSION['userid'])){
  // </div>
 -->  
 </form>
+
+<!-- 
+   SOCIAL MEDIA PLUGINS TO SHARE OUR PAGE. THIS IS AT THE BOTTOM
+-->
+
+  <!-- FACEBOOK start -->
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+  <div class="socialMediaFooter">
+  <p>
+  <!-- share button-->
+  <div class="fb-share-button" 
+    data-href="https://froothies.com" 
+    data-layout="button_count">
+  </div>
+  </p>
+  </div>
+   <!-- FACEBOOK end -->
+
 </body>
 </html>
 </body>
