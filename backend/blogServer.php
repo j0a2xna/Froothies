0d45b5b10b0e2602ca555a75745aa8c15816e03a
orquidiadr <oid2@njit.edu>
2020-05-10 16:46:03 -0400

    add blog.ini in frontend

--- backend/blogServer.php
+++ backend/blogServer.php
@@ -35,12 +35,12 @@
 
 
 			foreach($results as $test) {
-			/*	echo $test[0] . '<br>'; */
+				echo $test[0] . '<br>';
 				echo $test[1] . '<br>';
-			/*  echo $test[2] . '<br>';
+			    echo $test[2] . '<br>';
 				echo $test[3] . '<br>';
                 echo $test[4] . '<br>';
-                echo $test[5] . '<br>';*/
+                echo $test[5] . '<br>';
             }
             echo gettype($results);
 			return $results; 
