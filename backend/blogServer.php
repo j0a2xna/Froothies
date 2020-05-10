8e158497da1443f3b6b0b9832f7dbbcc1c24a2d8
orquidiadr <oid2@njit.edu>
2020-05-10 19:47:03 -0400

    changes so fe shows the tables

--- backend/blogServer.php
+++ backend/blogServer.php
@@ -5,22 +5,21 @@
     require_once('rabbitMQLib.inc');
 
     function connectDB(){
-        $db = 'allrecipes';
         $db_host = '10.0.0.31';
         $db_username = 'nemo';
         $db_password = 'dory123';
+        $db_name = 'allrecipes';
 
-        $conn = mysqli_connect($db_host, $db_username, $db_password, $db);
+        $conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 		if(!$conn){
 			die ("Failed to connect" . mysqli_connect_error());
 		}else{
-			echo "Successful connection" . PHP_EOL;
+			#echo "Successful connection" . PHP_EOL;
 		}
 		return $conn;
     }
 
     function requestProcessor($request){
-        var_dump($request);
         $username = $request['username'];
         $conn = connectDB();
         $sql = "SELECT username, recipe_name, fruit, veggies, protein, base FROM allrecipes";
@@ -36,20 +35,22 @@
 
 			foreach($results as $test) {
 				echo $test[0] . '<br>';
-				/*echo $test[1] . '<br>';
+				echo $test[1] . '<br>';
 			    echo $test[2] . '<br>';
 				echo $test[3] . '<br>';
                 echo $test[4] . '<br>';
-                echo $test[5] . '<br>'; 
-                */
+                echo $test[5] . '<br>';
                 echo "********************";
             }
-            echo gettype($results);
 			return $results; 
 		}
-        
-    }
-    $server = new rabbitMQServer("blog.ini","blogServer");
-	$server->process_requests('requestProcessor');
-    $server->send_request($row);
+        else{
+			echo "<h1>OOPS!</h1>";
+			echo "<h2> This is empty. Nobody has created any recipes! </h2>";
+		}
+
+    } 
+        $server = new rabbitMQServer("blog.ini","blogServer");
+        $server->process_requests('requestProcessor');
+        $server->send_request($row);
     ?>
--- frontend/blog.php
+++ frontend/blog.php
@@ -1,68 +1,59 @@
-<?php   
- //displays all recipes from database
-    session_start();
-    if(isset($_SESSION['userid'])){
-        
-    }else{
-        header("Location: ../frontend/index.php");
-    }
+<?php
+/* this page is for blogging */
+session_start();
+if(isset($_SESSION['userid'])){
 
-//required header
-    require_once('../backend/path.inc');
-    require_once('../backend/get_host_info.inc');
-    require_once('../backend/rabbitMQLib.inc');
+}else{
+	header("Location: ../frontend/index.php");
+}
 
-//grab current user from sessions
-    $username = $_SESSION['userid'];
+	require_once('../backend/path.inc');
+	require_once('../backend/get_host_info.inc');
+	require_once('../backend/rabbitMQLib.inc');
+	
+	$username = $_SESSION['userid'];
 
-//initialize new RMQ client
-    $client = new rabbitMQClient("blog.ini", "blogServer");
-    if(isset($_SESSION['userid'])){
-        $request['username'] = $username;
-        $response = $client -> send_request($request);
-        process_response($reponse);
-    }
+	$client = new rabbitMQClient("blog.ini", "blogServer");
+	if(isset($_SESSION['userid'])){
+		$request['username'] = $username;
+		$response = $client -> send_request($request);
+		process_response($response);
+	}
 
-    function process_response($response){
-        $username = $_SESSION['userid'];
-        //format and print navbar in html
-        echo '<div class="navbar">';
-            echo '<a href="../frontend/welcome.php"><i class="fa fa-fw fa-home"></i> Home </a>';
-            echo '<a href="../frontend/myaccount.php" id="acc"><i class="fa fa-fw fa-envelope"></i> My Account</a>';
-            echo '<a href="../frontend/logout.php" id="log"><i class="fa fa-fw fa-user"></i> Log Out</a> ';
-            echo '</div>';
+	function process_response($response){
+		$username = $_SESSION['userid'];
+		echo '<div class="navbar">';
+                echo '<a href="../frontend/welcome.php"><i class="fa fa-fw fa-home"></i> Home</a>';
+                echo '<a href="../frontend/myaccount.php" id="acc"><i class="fa fa-fw fa-envelope"></i> My Account</a>';
+                echo '<a href="../frontend/logout.php" id="log"><i class="fa fa-fw fa-user"></i> Log Out</a> ';
+                echo '</div>';
 
-        //echos greeting upon response
-        echo '<div class="greetingSuccess">';
-        echo "<h1> Hello, $username!!</h1>";
-        echo '<h2> You can see all YOUR recipes on <a href="../frontend/myaccount.php" id="acc"><i class="fa fa-fw fa-envelope"></i> My Account</a></h2>';
-        echo gettype($response);
-        echo "</div>";
-       
-        //if the user has no recipes, link to create one
-        if($response == "[empty response]"){
-            echo '<div class="greetingNOSuccess">';
-            echo 'OOPS! You have not created any recipes yet! <a href="../frontend/welcome.php"> Go here to create one <3 </a>';
-            echo "</div>";
-        }
+		// created div for the greeting because it was looping inside foreach
+		echo '<div class="greetingSuccess">';
+		echo "<h1> Hello, $username!!</h1>";
+		echo "<h2> You can see all your recipes below </h2>";
+		echo "</div>";
 
-        
+		 //if user does not have any recipes in their account, ask them to go make a smoothie in the welcome page
+                if($response == "[empty response]"){
+                        echo '<div class="greetingNOSuccess">';
+                        echo 'This is empty! <a href="../frontend/welcome.php"> Go here to create one smoothie. <3 </a>';
+                        echo "</div>";
+                }
 
-        foreach($response as $column){
-            echo "<h2>hello im in foreach<h2>";
-            echo '<div class="oneRecipe">';
-            echo "<h3>Recipe Name: " . $column[0] . "</h3>" . "<br>";
-                            echo "Fruits: " . $column[1] . '<br>';
-                            echo "Vegetables: " . $column[2] . '<br>';
-                            echo "Protein: " . $column[3] . '<br>';
-                            echo "Base: " . $column[4] . '<br>';
-                            echo "Made by: " . $column[5] . '<br>'; //will display other users usernames
-         
-
-
+		//for each row(array), get the value in the column
+		foreach($response as $column){
+				echo '<div class="oneRecipe">';
+				echo "<h3>Made By: " . $column[0] . "</h3>" . "<br>";
+                echo "Recipe Name: " . $column[1] . '<br>';
+                echo "Fruit: " . $column[2] . '<br>';
+                echo "Vegetable: " . $column[3] . '<br>';
+                echo "Protein: " . $column[4] . '<br>';
+                echo "Base: " . $column[5] . '<br>';
 ?>
-    <!-- facebook button -->
-    <div id="fb-root"></div>
+
+				<!-- facebook button -->
+                <div id="fb-root"></div>
 				  <script>(function(d, s, id) {
 				    var js, fjs = d.getElementsByTagName(s)[0];
 				    if (d.getElementById(id)) return;
@@ -76,7 +67,7 @@
 				  <div class="socialMediaMyAccount">
 				    <p>
 				      <div class="fb-share-button" 
-				        data-href="https://froothies.com" 
+				        data-href="https://froothies.net" 
 				        data-layout="button_count">
 				      </div>
     
@@ -84,7 +75,7 @@
 					<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 						<a href="https://twitter.com/share?ref_src=twsrc%5Etfw"
 							class="twitter-share-button"
-							data-text="Take a look at this awesome smoothie recipe! <?php echo ' Recipe Name: ' . $column[0] . '. Fruits: ' . $column[1] . '. Vegetables: ' . $column[2] . '. Protein: ' . $column[3] . '. Base: ' . $column[4];?>"
+							data-text="Take a look at this awesome smoothie recipe! <?php echo ' Made By: ' . $column[0] . '. Recipe Name: ' . $column[1] . '. Fruit: ' . $column[2] . '. Vegetable: ' . $column[3] . '. Protein: ' . $column[4] . '. Base: ' . $column[5];?>"
 							data-hashtags="froothies" 
 							data-related="" 
 							data-show-count="true">Tweet
@@ -94,9 +85,10 @@
 			</div></div>
 		<?php	}
 	} ?>
+
 <html>
 	<head>
-	<title>My Account</title>
+	<title>Blogging</title>
 	<link rel="stylesheet" href="../frontend/css/nav.css">
 	<link rel="stylesheet" type="text/css" href="../frontend/css/style.css">
 	<meta property="og:description" content="Tutorials, thoughts, and random stuff ♥" />
@@ -137,4 +129,3 @@
       
     </body>
 </html>
-
